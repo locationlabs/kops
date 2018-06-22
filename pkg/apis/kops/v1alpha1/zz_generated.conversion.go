6521216eb0b70ab629ae93c5802d56af294d0cf8
Chris Phillips <chris.phillips@locationlabs.com>
2018-06-22 10:13:30 -0700

    allow overriding subnet tags in the NetworkSpec

--- pkg/apis/kops/v1alpha1/zz_generated.conversion.go
+++ pkg/apis/kops/v1alpha1/zz_generated.conversion.go
@@ -2776,6 +2776,7 @@ func Convert_kops_LoadBalancerAccessSpec_To_v1alpha1_LoadBalancerAccessSpec(in *
 }
 
 func autoConvert_v1alpha1_LyftVPCNetworkingSpec_To_kops_LyftVPCNetworkingSpec(in *LyftVPCNetworkingSpec, out *kops.LyftVPCNetworkingSpec, s conversion.Scope) error {
+	out.SubnetTags = in.SubnetTags
 	return nil
 }
 
@@ -2785,6 +2786,7 @@ func Convert_v1alpha1_LyftVPCNetworkingSpec_To_kops_LyftVPCNetworkingSpec(in *Ly
 }
 
 func autoConvert_kops_LyftVPCNetworkingSpec_To_v1alpha1_LyftVPCNetworkingSpec(in *kops.LyftVPCNetworkingSpec, out *LyftVPCNetworkingSpec, s conversion.Scope) error {
+	out.SubnetTags = in.SubnetTags
 	return nil
 }
 
